f9452b41f7202dd685985e984ea17f2cf2ffb031
unknown <Tupsu@.(none)>
2013-04-30 02:18:29 +0300

    Made code easier to read

--- Listeners/NavButtonListeners.js
+++ Listeners/NavButtonListeners.js
@@ -2,23 +2,30 @@ $(document).ready(function() {
     
     // Click handler for recieved tasks button (decision)
     $('#recievedTasks').click(function() {
-        // Clear tasklist
-        $('#taskList').find('li').remove();
+        clearTaskList();
         // Get tasks
         getAvailableTasks(localStorage.getItem('token'), "ACTIVITY", "T._state = TaskState.READY");
     });
     
     // Click handler for handled tasks  button (notify)
     $('#handledTasks').click(function() {
-        $('#taskList').find('li').remove();
+        clearTaskList();
         // Get notification tasks
         getAvailableTasks(localStorage.getItem('token'), "NOTIFICATION", "T._state = TaskState.READY");
     });
     
     // Click handler for create task button (init)
     $('#createTask').click(function() {
-        $('#taskList').find('li').remove();
+        clearTaskList();
         // Get init tasks
         getAvailableTasks(localStorage.getItem('token'), "INIT", "");
     });
 });
+
+/*
+ * Removes every li element from tasklist
+ *
+ */
+function clearTaskList() {
+    $('#taskList').find('li').remove();
+}
