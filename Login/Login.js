feb28f706148749973c5a63de02c21522d7a78fd
unknown <Tupsu@.(none)>
2013-05-01 01:45:14 +0300

    FIXED: TaskList didn't show any tasks after login. Tasks were loaded only after manual page refresh.

--- Login/Login.js
+++ Login/Login.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     $(document).on('click', '#btn_login', function() {
-        var username = $('#username').val();
-        var password = $('#password').val();   
-        authenticate(username, password);
-        $.mobile.changePage('../index.html');
+        if (localStorage.getItem('token') == null) {
+            var username = $('#username').val();
+            var password = $('#password').val();   
+            authenticate(username, password);
+        }
+        $.mobile.changePage('../');
     });
 });
