8963e025f8329078dbaac4f187597fd4f664a92e
unknown <Tupsu@.(none)>
2013-11-25 19:04:44 +0200

    Included licence.

--- Login/CheckLogin.js
+++ Login/CheckLogin.js
@@ -1,4 +1,25 @@
-// Wait until page is fully loaded. $(document).ready() causes incorrect page loading when using changePage(arg) on some browsers
+/** 
+ 	This file is part of TaskManager.
+
+    TaskManager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TaskManager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with TaskManager.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+/**
+ * Check if user is logged in and either update task list UI or load login page.
+ * @author Teemu Räsänen
+ */
+// Wait until page is fully loaded. $(document).ready() causes incorrect page loading when using changePage(arg) on some browsers (WebKit)
 $(window).load(function() {
     "use strict";
     
